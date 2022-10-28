63f343032aec3ef5ac69d3e7b6ff32d435a01135
Daniil Lemeshevski <dan4ik599@gmail.com>
2022-10-28 13:33:03 +0300

    add typescript and fix style

--- weatherWidgetFront/src/App.tsx
+++ weatherWidgetFront/src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
-import reactLogo from './assets/react.svg'
+import { ReactLogo } from './assets/ReactLogo'
 import './App.css'
 import classes from './style.module.scss'
 
@@ -15,7 +15,8 @@ function App() {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src={<reactLogo/>} className="logo react" alt="React logo" /> */}
+          <ReactLogo />
         </a>
       </div>
       <h1>Vite + React</h1>
